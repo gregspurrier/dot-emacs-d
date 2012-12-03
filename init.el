45811e0d3e5956f69451ba0d3392d032df27053a
Greg Spurrier <gspurrier@linkedin.com>
2012-12-03 08:39:42 -0800

    First cut

---
+++ init.el
@@ -0,0 +1,56 @@
+;; Packages managed by package.el
+;; ------------------------------
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(defvar my-packages '(highlight-parentheses 
+		      paredit
+		      fill-column-indicator
+		      solarized-theme 
+		      zenburn-theme
+		      htmlize
+		      markdown-mode)
+  "A list of packages to ensure are installed at launch.")
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+
+;; Packages installed locally
+;; --------------------------
+
+;; General Customizations
+;; ----------------------
+(setq make-backup-files nil)
+(setq auto-save-default nil)
+(global-auto-revert-mode t)
+(setq-default tab-width 2)
+(setq-default indent-tabs-mode nil)
+(setq inhibit-startup-message t)
+
+;; Customize the way that Emacs looks
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(column-number-mode t)
+
+(if (window-system)
+    (progn
+      (load-theme 'zenburn t)
+      (set-default-font "Inconsolata-14")
+      (set-frame-height (selected-frame) 48)))
+
+;; Meta key bindings for Mac OS X
+(setq mac-option-modifier 'hyper)
+(setq mac-command-modifier 'meta)
+
+;; Mode Hooks
+;; ----------
+
+
